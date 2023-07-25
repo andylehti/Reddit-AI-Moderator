e5af37b13eaa8212a775472769ff9b4238302bbc
Andrew Lehti <53940535+andylehti@users.noreply.github.com>
2023-07-25 11:13:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+sudo apt install python3.10-venv
+
 # Install necessary system packages
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y python3-pip python3-dev cron
 sudo apt-get install -y libsm6 libxext6 libxrender-dev
 sudo apt-get install -y nodejs npm
+
+#Install packages with pip
 pip install nltk transformers vaderSentiment
+pip install fuzzywuzzy pytesseract
 
 # Add Python user bin to PATH
 echo "export PATH=$PATH:/home/$USER/.local/bin" >> ~/.bashrc
@@ -25,4 +30,9 @@ pip install -r requirements.txt
 # Install the Python and Node packages
 pip install praw easyocr nltk
 npm install nsfwjs
+
+curl -O https://raw.githubusercontent.com/andylehti/Reddit-AI-Moderator/main/configure.sh
+chmod +x configure.sh
+./configure.sh
+
 #!/bin/bash
